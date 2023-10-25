8a415f62738513a1930294ac940cfadc436f0ba1
Ando Rafanomezantsoa <arafanomezantsoa@easyvista.com>
2023-10-25 10:58:25 +0200

    page de connexion et d'inscription

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\LoginController@login');
+
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register', 'Auth\RegisterController@register');
