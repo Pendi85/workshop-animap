f64607af5027989ea88343b525fa5204dc3194d7
Ando Rafanomezantsoa <arafanomezantsoa@easyvista.com>
2023-10-26 09:51:19 +0200

    update vite version and update route for authentification

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+<<<<<<< Updated upstream
+=======
+
+Route::get('/map', function () {
+    return view('map');
+});
+
+/*
+>>>>>>> Stashed changes
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login');
 
 Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('/register', 'Auth\RegisterController@register');
+*/
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
 
 Route::get('/species', function () {
     return view('species');
